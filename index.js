85bf225fa5c163716ffe0c47fe31796f575f1b2b
Fokhrul Islam <shahidcse12@gmail.com>
2022-05-02 22:56:02 +0600

    load data API

--- index.js
+++ index.js
@@ -13,15 +13,29 @@ app.use(express.json());
 
 
 
-const uri =   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q9zoc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('DB connected');
-  // perform actions on the collection object
-  client.close();
-});
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q9zoc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+
+        const productCollection = client.db('gadgetMania').collection('product');
+       app.get('/inventory', async(req, res)=>{
+        const query = {};
+        const cursor = productCollection.find(query);
+        const products = await cursor.toArray();
+        res.send(products);
+       })
+    }
+    finally{
+
+    }
+
+}
+
+run().catch(console.dir)
 
 app.get('/', (req, res)=>{
     res.send('Running Server');
