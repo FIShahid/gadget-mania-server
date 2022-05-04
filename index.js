c0204d8c03bee22ab72ecdf52d00b8683a0e8187
Fokhrul Islam <shahidcse12@gmail.com>
2022-05-04 20:32:59 +0600

    single product load

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require ('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,8 +27,15 @@ async function run(){
         const cursor = productCollection.find(query);
         const products = await cursor.toArray();
         res.send(products);
-       })
-    }
+       });
+
+       app.get('/inventory/:id', async(req,res)=>{
+           const id =req.params.id;
+           const query={_id: ObjectId(id)}
+       const product = await productCollection.findOne(query);
+        res.send(product)
+    })
+}
     finally{
 
     }
