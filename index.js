6d66a18ff21691626af4373d16d77b4deec51feb
Fokhrul Islam <shahidcse12@gmail.com>
2022-05-07 02:07:33 +0600

    delete item

--- index.js
+++ index.js
@@ -65,6 +65,15 @@ async function run(){
         const result = await productCollection.insertOne(newProduct);
         res.send(result);
     });
+
+    // DELETE
+    app.delete('/inventory/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await productCollection.deleteOne(query);
+        res.send(result);
+    });
+
 }
     finally{
 
